6297e1e8ae2cac10022783caee2452cddddae665
tgaychey <tgaychey@gmail.com>
2019-11-21 01:09:33 -0500

    Full design refactor.

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('working');
+/* const blogposts = document.getElementById('recent-blogposts');
+
+const data = fetch('http://blog.tenzinggaychey.com/wp-json/wp/v2/posts')
+  .then(response => response.json())
+  .then(posts => {
+    blogposts.innerHTML += `<ul style="list-style: none; margin: 0; padding: 0;">`;
+    posts.forEach(post => {
+      blogposts.innerHTML += `<li style="list-style: none;"><a href="${
+        post.link
+      }">${post.title.rendered}</a></li>`;
+    });
+    blogposts.innerHTML += `</ul>`;
+  });
+ */
